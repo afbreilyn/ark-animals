81a19c034c2d41f280131f12a8120e4700dec1aa
aaron foster bresley <aaron.foster@breilyn.com>
2020-09-24 00:35:57 -0400

    Adds ship-it. Thx ALV && WS

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Working through this guide first before i mess things up :)
 TODO: 
 [x] finish this [blog tutorial](https://github.com/spring-guides/tut-spring-boot-kotlin) (afb - 23_09_20)
 [x] convert domain concepts to 2x2 (afb - 24_09_20)
+[ ] use react instead of mustache
 [ ] add and display cards
 [ ] improve test coverage
-[ ] use react instead of mustache
 [ ] styling
 [ ] add e2e testing
 [ ] add security
---
+++ scripts/ship-it.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+function set_bash_fail_on_error() {
+    set -e
+}
+
+function go_to_root_directory() {
+    root_directory=$(git rev-parse --show-toplevel)
+
+    cd "$root_directory"
+}
+
+function run_tests() {
+  run_gradle_tests
+}
+
+function run_gradle_tests() {
+    ./gradlew test
+}
+
+function push_code() {
+    git push
+}
+
+function main() {
+    set_bash_fail_on_error
+    go_to_root_directory
+
+    run_tests
+
+    push_code
+}
+
+main "$@"
