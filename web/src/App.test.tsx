5b6432821b4e2fe8ed528353ac89915693c822f1
aaron foster bresley <aaron.foster@breilyn.com>
2020-10-14 17:57:29 -0400

    Adds a basic form to the App component

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import axios from 'axios';
+import {act} from "react-dom/test-utils";
+
+function flushPromises(): Promise<any> {
+  return new Promise(resolve => setImmediate(resolve));
+}
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('<App />', () => {
+  it('displays a name (smoke test)', () => {
+    const { getByText } = render(<App/>)
+    expect(getByText('cards!')).toBeInTheDocument()
+  })
+
+  describe('the form', () => {
+    it('displays a form', () => {
+      const { queryByTestId } = render(<App/>)
+
+      expect(queryByTestId('new-card-form')).toBeTruthy()
+    })
+
+    it('displays a text input and label', () => {
+      const { getByLabelText, getByTestId } = render(<App/>)
+
+      expect(getByLabelText('Sticky note text')).toBeInTheDocument()
+      expect(getByTestId('sticky-text-input')).toBeTruthy()
+    })
+
+    it('can submit the form', async () => {
+      mockedAxios.get.mockResolvedValue({ data: "hello from axios" });
+
+      const { getByTestId, getByText } = render(<App/>)
+      const inputEl = getByTestId('sticky-text-input') as HTMLInputElement
+
+      inputEl.value = 'just some text'
+
+      fireEvent.click(getByTestId('submit-button'))
+
+      await act(flushPromises);
+
+      expect(getByText("hello from axios", {exact: false})).toBeInTheDocument();
+    })
+  })
+})
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios, { AxiosResponse } from 'axios'
 
-function App() {
+
+export const App = () => {
+  const [greeting, setGreeting] = useState("")
+
+  const mySubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    axios.get('/api/someRandomEndpoint')
+      .then((response: AxiosResponse<string>) => {
+        setGreeting(response.data)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>cards!</h1>
+      <form data-testid="new-card-form" onSubmit={e => mySubmit(e)}>
+        <label htmlFor="sticky-text">Sticky note text</label>
+        <input
+          id="sticky-text"
+          data-testid="sticky-text-input"
+        />
+        <button type="submit" data-testid="submit-button">Submit</button>
+      </form>
+      <h1>
+        {greeting}
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
